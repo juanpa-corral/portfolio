4a6be1bec55e5664ef872600717f220f7d68273a
Juan pablo corral <78913361+juanpa-corral@users.noreply.github.com>
2025-05-01 14:11:16 -0500

    Add cards and button function

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,8 +2,17 @@ import React from "react";
 import { words } from "../constans/index.js";
 import Button from "../components/Button.jsx";
 import HeroExperience from "../components/HeroModels/HeroExperience.jsx";
+import { useGSAP } from "@gsap/react";
+import gsap from 'gsap'
+import AnimatedCounter from "../components/AnimatedCounter.jsx";
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.fromTo('.hero-text h1',
+            {y: 50, opacity:0},
+            {y: 0, opacity: 1, stagger: 0.2, duration: 1, ease: 'power2.inOut'}
+        )
+    })
     return(
         <section id="hero" className="relative overflow-hidden">
             <div className="absolute top-0 left-0 z-10">
@@ -43,6 +52,7 @@ const Hero = () => {
                 </figure>
                 
             </div>
+            <AnimatedCounter />
         </section>
     )
 }
