38eed99407d40267e9bbe69bebd646ce86d7e3dd
Juan pablo corral <78913361+juanpa-corral@users.noreply.github.com>
2025-05-01 23:10:35 -0500

    Update showcase

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react"
-import {gsap} from 'gsap'
+import { gsap } from 'gsap'
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 
@@ -11,12 +11,32 @@ const ShowcaseSection = () => {
     const project2Ref = useRef(null);
     const project3Ref = useRef(null);
 
+    
     useGSAP(() => {
+        const projects = [project1Ref.current, project2Ref.current, project3Ref.current]
+        projects.forEach((card, index) => {
+            gsap.fromTo(
+                card,
+                {
+                    y: 50, opacity: 0
+                },
+                {
+                    y: 0,
+                    opacity: 1,
+                    duration: 1,
+                    delay: 0.3 * (index + 1),
+                    scrollTrigger: {
+                        trigger: card,
+                        start: 'top bottom-=100'
+                    }
+                }
+            )
+        });
         gsap.fromTo(sectionRef.current,
             { opacity: 0},
             {opacity: 1, duration: 1.5}
         )
-    }, [])
+    }, []);
 
     return (
         <section id="work" ref={sectionRef} className="app-showcase">
